f0283fba02437ace123714a5aa704344cd34d27b
orphanofAryavath <46354666+orphanofAryavath@users.noreply.github.com>
2019-03-01 13:47:28 +0530

    Delete manas.py

--- mysite/manas.py
+++
@@ -1,6 +0,0 @@
-                                                    
-from bs4 import BeautifulSoup
-driver=webdriver.PhantomJS(r'Z:\manas1\phantomjs.exe')
-url = 'https://in.global.nba.com/playerindex/?_ga=2.71736659.1354572313.1551276365-121493775.1548256063'
-driver.get(url)
-print (driver.page_source)
